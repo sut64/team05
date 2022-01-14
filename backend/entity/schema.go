383acdbb55674d3bb5de5535be90e6078e4edaa3
chinatip7799 <85872909+chinatip7799@users.noreply.github.com>
2022-01-15 00:55:35 +0700

    fixed issue-4

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -6,41 +6,44 @@ import (
 	"gorm.io/gorm"
 )
 
-type Customer struct {
+type Employee struct {
 	gorm.Model
-	Name           string
-	ID_Customer    string `gorm:"uniqueIndex"`
-	Password       string
-	RepairRequests []RepairRequest `gorm:"foreignKey:CustomerID"`
+	Name string
+	// 1 employee can create many Workrecive
+	Workrecives []Workrecive `gorm:"foreignkey:WorkreciveID`
+	// 1 employee can create many RecieptHistory
+	RecieptHistories []RecieptHistory `gorm:"foreignkey:RecieptHistoryID`
 }
-type RepairType struct {
+
+type Workrecive struct {
 	gorm.Model
-	Name           string
-	RepairRequests []RepairRequest `gorm:"foreignKey:CustomerID"`
+	WorkCode     string
+	Detail       string
+	Wages        float32
+	FinishedDate time.Time
+
+	EmployeeID       *uint
+	Employee         Employee
+	RecieptHistories []RecieptHistory `gorm:"foreignkey:RecieptHistoryID`
 }
-type Urgency struct {
+
+type PaidBy struct {
 	gorm.Model
-	Name           string
-	RepairRequests []RepairRequest `gorm:"foreignKey:CustomerID"`
+	Name             string
+	RecieptHistories []RecieptHistory `gorm:"foreignkey:RecieptHistoryID`
 }
-type RepairRequest struct {
+
+type RecieptHistory struct {
 	gorm.Model
+	RecipetID    string
+	RecieptPrice uint
+	RecieptDate  time.Time
 
-	Device      string
-	lifetime    uint
-	issue       string
-	RequestDate time.Time
-
-	CustomerID *uint
-	Customer   Customer `gorm:"references:id"`
-
-	RepairTypeID *uint
-	RepairType   RepairType `gorm:"references:id"`
-
-	UrgencyID *uint
-	Urgency   Urgency `gorm:"references:id"`
-
-	RepairRequstID *uint
-	RepairRequest  []RepairRequest `gorm:"references:id"`
+	EmployeeID   *uint
+	Employee     Employee
+	WorkreciveID *uint
+	Workrecive   Workrecive
+	PaidByID     *uint
+	PaidBy       PaidBy
 }
 

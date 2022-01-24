81e30855bb9b86ea78d22b4210897823f7df7953
Rachapon23 <ohm32070@gmail.com>
2022-01-24 16:40:01 +0700

    snapshot-3 for issue-47

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -93,6 +93,8 @@ type WorkRecive struct {
 	RecieptHistories []RecieptHistory `gorm:"foreignKey:WorkreciveID"`
 	// foreignkey to PartsPurchase
 	PartsPurchases []PartsPurchase `gorm:"foreignKey:WorkreciveID"`
+	// (ohm) foreignkey to Warrantee
+	Warrantee Warrantee `gorm:"references:ID"`
 }
 
 type PaidBy struct {

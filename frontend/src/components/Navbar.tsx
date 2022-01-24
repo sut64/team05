e535f000bcffa25165c2505d04321bbcd9e4ef49
Rachapon23 <ohm32070@gmail.com>
2022-01-24 16:37:18 +0700

    snapshot-4 for issue-51

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,40 +1,166 @@
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu" 
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
-const useStyles = makeStyles((theme) => ({
-    root: {flexGlow: 1},
-    menuButton: {marginRight: theme.spacing(2)},
-    title: {flexGrow: 1},
-    navlink: {color: "white", textDecoration: "none"}
-}));
+//---------Icon----------- 
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import HomeIcon from '@material-ui/icons/Home';
+import BuildIcon from '@material-ui/icons/Build';
 
-function Navbar() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <AppBar>
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="menu"
-                    >
-                        <MenuIcon/>
-                    </IconButton>
+const drawerWidth = 240;
 
-                    <Link className={classes.navlink} to="/">
-                        <Typography variant="h6" className={classes.title}>
-                            ระบบแจ้งซ่อมคอมพิวเตอร์
-                        </Typography>
-                    </Link>
-                    
-                </Toolbar>
-            </AppBar>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+    a: {
+      textDecoration: "none",
+      color: "inherit",
+    },
+  }),
+);
+
+export default function NavBar() {
+  const menu = [
+    { name: "หน้าแรก", icon: <HomeIcon />, path: "/" },
+    { name: "ระบบที่ 1", icon: <LabelImportantIcon />, path: "/" },
+    { name: "ระบบที่ 2", icon: <LabelImportantIcon />, path: "/" },
+    { name: "ระบบที่ 3", icon: <LabelImportantIcon />, path: "/" },
+    { name: "ระบบที่ 4", icon: <LabelImportantIcon />, path: "/" },
+    { name: "ระบบที่ 5", icon: <LabelImportantIcon />, path: "/" },
+    { name: "ออกจากระบบ", icon: <ExitToAppIcon />, path: "/SignIn" },
+  ];
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            ระบบแจ้งซ่อมคอมพิวเตอร์
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <BuildIcon />พนักงานรร้านซ่อมคอมพิวเตอร์
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
         </div>
-    );
+        <Divider />
+        <List>
+          {menu.map((item) => (
+            <Link to={item.path} key={item.name} className={classes.a}>
+              <ListItem button key={item.name}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.name} />
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
 }
-
-export default Navbar;

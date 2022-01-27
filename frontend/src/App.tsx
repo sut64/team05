476d90d47867a091b032fa60cc5d3f41af5f8e67
Boom.Phattarapong <61021302+boomertnt210943@users.noreply.github.com>
2022-01-27 16:06:49 +0700

    update userinterface partspurchase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,14 +6,18 @@ import Warrantee from "./components/Warrantee";
 import HomeEmployee from './components/Home_employee';
 import HomeCustomer from './components/Home_Customer';
 import RepairRequest from './components/RepairRequest';
+import PartsPurchase from './components/PartsPurchase';
+import CreatePartsPurchase from './components/CreatePartsPurchase';
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<HomeEmployee />} />
+          <Route path="/" element={<HomeEmployee />} /> 
           <Route path="/customer" element={<HomeCustomer />} />
           <Route path="/RepairRequest" element={<RepairRequest />} />
+          <Route path="/PartsPurchase" element={<PartsPurchase />} />
+          <Route path="/PartsPurchase/create" element={<CreatePartsPurchase />} />
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/warrantee" element={<Warrantee/>}/>
           <Route path="/warrantee/create" element={<WarranteeCreate/>}/>

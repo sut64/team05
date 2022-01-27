317f3607220b69691bdca5e8b57aab6a1ec439b3
bankvery007 <bunyarithvery007@hotmail.com>
2022-01-27 17:38:53 +0700

    update App and navbar_employee

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from './components/SignIn';
 import WarranteeCreate from "./components/WarranteeCreate";
@@ -6,7 +6,20 @@ import Warrantee from "./components/Warrantee";
 import HomeEmployee from './components/Home_employee';
 import HomeCustomer from './components/Home_Customer';
 import RepairRequest from './components/RepairRequest';
+import NavBar_Employee from "./components/NavBar_employee";
 function App() {
+
+  const [token, setToken] = React.useState<String>("");
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+
+  if (!token) {
+    return <SignIn />;
+  }
   return (
     <Router>
       <div>
